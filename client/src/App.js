ced9150f51f626b87a6ed86f33f89b218ad1062c
Thuan Phat <thuanphat276@gmail.com>
2022-08-03 10:06:39 +0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.scss';
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline title">
+      Hello world!
+    </h1>
+  )
+}
